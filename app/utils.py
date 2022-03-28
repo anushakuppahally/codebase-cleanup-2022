3fefa427ffe8de261aaa2d7dc07e6c7029c10358
anushakuppahally <aak154@georgetown.edu>
2022-03-28 17:58:27 -0400

    commit on 'simpler'

---
+++ app/utils.py
@@ -0,0 +1,12 @@
+def to_usd(my_price):
+    """
+    This is a docstring. This tells us what the function is about
+    What its responsibilities are
+    What the parameters are about
+    What datatypes the parameters are
+    What this function will return
+    Example of invoking the function
+
+    Invoke like this: to_use(9.9999)
+    """
+    return '${:,.2f}'.format(my_price)
