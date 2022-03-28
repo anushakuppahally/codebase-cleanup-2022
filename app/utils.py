4670bde1ef26eed213fc039aa861a6c38d4ff584
anushakuppahally <aak154@georgetown.edu>
2022-03-28 18:12:20 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,6 @@
+from json.tool import main
+
+
 def to_usd(my_price):
     """
     This is a docstring. This tells us what the function is about
@@ -10,3 +13,21 @@ def to_usd(my_price):
     Invoke like this: to_use(9.9999)
     """
     return '${:,.2f}'.format(my_price)
+
+
+
+#if this code is in the global scope
+#of a file we're trying to import from
+#it will throw errors when we try to run those 
+#price = input("Please choose a price like 4.9999: ")
+#print(to_usd(float(price)))
+
+if __name__ == "__main__":
+    #nesting the code in the main conditional will
+    #allow other scripts to cleanly import functions from this file
+    #without running this code
+    # 
+    # this code still gets run when we invoke the script from the command line  
+    price = input("Please choose a price like 4.9999: ")
+    print(to_usd(float(price)))
+
