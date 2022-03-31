fe7848c1a800cc1320de64bb56399f655b58dc13
anushakuppahally <aak154@georgetown.edu>
2022-03-30 21:46:26 -0400

    testing updates

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -9,7 +9,7 @@ from app.utils import to_usd #from foldername.filename import function
 
 def test_to_usd(): #test function should be called test_something
     #it rounds to 2 decimal places and have a $
-    assert to_usd(0.455555) == "0.46"
+    assert to_usd(0.455555) == "$0.46"
 
     #if large number, should use thousands separator:
     assert to_usd(123454645.98) == "$123,454,645.98"
