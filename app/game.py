f6b88cb7a7512d47b34b1ff90845bf4d4206e766
Anusha Kuppahally <92832453+anushakuppahally@users.noreply.github.com>
2022-03-30 21:33:08 -0400

    Testing (#4)
    * Enter
    
    * testing updates

--- app/game.py
+++ app/game.py
@@ -1,49 +1,79 @@
-
-
-
-
 from random import choice
 
-valid_selections = ["rock", "paper", "scissors"] #only have to update in one place
+def determine_winner(user_choice, computer_choice):
+    #return "paper"
+    winners = {
+        "rock": {
+            "rock": None,
+            "paper": "paper",
+            "scissors": "rock",
+        },
+        "paper": {
+            "rock": "paper",
+            "paper": None,
+            "scissors": "scissors",
+        },
+        "scissors": {
+            "rock": "rock",
+            "paper": "scissors",
+            "scissors": None,
+        }
+    }
+    winning_choice = winners[user_choice][computer_choice]
+    return winning_choice
+    #return "OOPS"
+    
 
-#
-# USER SELECTION
-#
+if __name__=="__main__":
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in valid_selections :
-    print("OOPS, TRY AGAIN")
-    exit()
+    valid_selections = ["rock", "paper", "scissors"] #only have to update in one place
 
-#
-# COMPUTER SELECTION
-#
+    #
+    # USER SELECTION
+    #
 
-c = choice(valid_selections)
-print("COMPUTER CHOICE:", c)
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in valid_selections :
+        print("OOPS, TRY AGAIN")
+        exit()
 
-#
-# DETERMINATION OF WINNER
-#
+    #
+    # COMPUTER SELECTION
+    #
 
-if u == "rock" and c == "rock":
-    print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
+    c = choice(valid_selections)
+    print("COMPUTER CHOICE:", c)
 
-elif u == "paper" and c == "rock":
-    print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
+    #
+    # DETERMINATION OF WINNER
+    #
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
-    print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
+    # if u == "rock" and c == "rock":
+    #     print("It's a tie!")
+    # elif u == "rock" and c == "paper":
+    #     print("The computer wins")
+    # elif u == "rock" and c == "scissors":
+    #     print("The user wins")
+
+    # elif u == "paper" and c == "rock":
+    #     print("The computer wins")
+    # elif u == "paper" and c == "paper":
+    #     print("It's a tie!")
+    # elif u == "paper" and c == "scissors":
+    #     print("The user wins")
+
+    # elif u == "scissors" and c == "rock":
+    #     print("The computer wins")
+    # elif u == "scissors" and c == "paper":
+    #     print("The user wins")
+    # elif u == "scissors" and c == "scissors":
+    #     print("It's a tie!")
+
+    winner = determine_winner(u,c)
+    if winner == u:
+        print("YOU WON")
+    elif winner == c:
+        print("COMPUTER WON")
+    else
+        print("TIE")
---
+++ tests/utils_test.py
@@ -0,0 +1,19 @@
+#each test file must be called test_something.py or something_test.py
+#each directory must be called test/tests/something with test 
+#this is the "tests/util_test.py" file
+
+# def test_answer():
+#     assert inc(3) == 5 
+
+from app.utils import to_usd #from foldername.filename import function 
+
+def test_to_usd(): #test function should be called test_something
+    #it rounds to 2 decimal places and have a $
+    assert to_usd(0.455555) == "0.46"
+
+    #if large number, should use thousands separator:
+    assert to_usd(123454645.98) == "$123,454,645.98"
+    
+
+#run pytest from command line
+#for tests to find the code properly, create new file in root directory - helps pytest find tests
